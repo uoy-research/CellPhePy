9b3db0986ccd57d122402db9e44fe2b03bb412ed
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-03-04 13:00:24 +0000

    Updated Sphinx config with correct author name

--- docs/conf.py
+++ docs/conf.py
@@ -18,9 +18,9 @@ sys.path.insert(0, os.path.abspath("../src/"))
 
 # -- Project information -----------------------------------------------------
 
-project = "ai-python docs"
-copyright = "2022, Daniel Ciborowski"
-author = "Daniel Ciborowski"
+project = "CellPhePy"
+copyright = "2024, Stuart Lacy"
+author = "Stuart Lacy"
 
 # The full version, including alpha/beta/rc tags
 release = "0.1.0"
