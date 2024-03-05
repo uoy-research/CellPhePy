54b3844886cc125af7eb9ad2aeacb83210b5dbc2
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-03-05 11:08:12 +0000

    Added remove_predicted_seg_errors (#19)
    Also setup the `segmentation` sub-package with imports, so users can
    write `from cellphe.segmentation import remove_predicted_seg_errors`

---
+++ src/cellphe/segmentation/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Functions related to predicting and removing segmentation errors.
+"""
+
+from __future__ import annotations
+
+from cellphe.segmentation.seg_errors import remove_predicted_seg_errors
