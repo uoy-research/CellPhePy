bafa27f0b2b35305c19e0e03feaec61ee67aaaf4
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-05-07 12:11:22 +0100

    Added test for Haar approx on real image

--- tests/test_extract_features_helpers.py
+++ tests/test_extract_features_helpers.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 import pytest
+from PIL import Image
 
 from cellphe.features.frame import *
 
@@ -257,6 +258,16 @@ def test_haar_approximation():
     input = np.arange(1, 25).reshape(6, 4, order="F")
     expected = np.array([[4.5, 16.5], [6.5, 18.5], [8.5, 20.5]])
     output = haar_approximation(input)
+    assert output == pytest.approx(expected)
+
+
+def test_haar_real_image():
+    # Test Haar wavelet approximation using Pywavelets as used in this
+    # package against the hand-rolled version in the original CellPhe
+    image = Image.open("tests/resources/frame.tif")
+    image = np.array(image)
+    output = haar_approximation(image)
+    expected = np.genfromtxt("tests/resources/haar_output.csv", delimiter=",")
     assert output == pytest.approx(expected)
 
 
