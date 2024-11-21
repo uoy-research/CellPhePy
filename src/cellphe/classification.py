7318dfa96ee2978c187d36c0bf82428d789b5c93
Stuart Lacy <stuart.lacy@york.ac.uk>
2024-11-21 21:12:17 +0000

    todo

--- src/cellphe/classification.py
+++ src/cellphe/classification.py
@@ -52,6 +52,8 @@ def classify_cells(train_x: pd.DataFrame, train_y: np.array, test_x: pd.DataFram
     # manually. But then it returns the predictions unlabelled so then need to
     # reapply label
     # Pylint doesn't recognise the predict method and le_ attributes
+    # TODO: Could probably individual_preds = use ensemble.transform(test_x)
+    # And get model_names from list(ensemble.named_estimators.keys())
     # pylint: disable=no-member
     ensemble_preds = ensemble.predict(test_x)
     individual_preds = np.array([ensemble.named_estimators_[x].predict(test_x) for x in model_names])
