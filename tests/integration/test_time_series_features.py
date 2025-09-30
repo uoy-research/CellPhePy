044ccce5cccb5a72a93efe2cba4c1d35efe87a6e
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-09-30 20:47:50 +0100

    Integration tests checks for number of columns
    Confirm that when a dataset has some values set to NA, the correct
    number of columns is still returned

--- tests/integration/test_time_series_features.py
+++ tests/integration/test_time_series_features.py
@@ -47,7 +47,7 @@ def test_time_series_features(frame_df, ts_df):
     assert_frame_equal_extended_diff(ts_df.reset_index(drop=True), output.reset_index(drop=True))
 
 
-def test_time_series_features_short_series(frame_df):
+def test_time_series_features_short_series(frame_df, ts_df):
     # R package uses 'xpos' and 'ypos' - Python package uses 'x' and 'y'
     df = frame_df.loc[frame_df["FrameID"] < 6]
     output = time_series_features(df)
@@ -56,9 +56,10 @@ def test_time_series_features_short_series(frame_df):
     wavelet_regex = re.compile("_l3_")
     wavelet_cols = list(filter(wavelet_regex.search, output.columns.values))
     assert output[wavelet_cols].isna().all().all()
+    assert output.shape[1] == ts_df.shape[1]  # Correct # cols
 
 
-def test_time_series_features_1_short_1_long(frame_df):
+def test_time_series_features_1_short_1_long(frame_df, ts_df):
     # Test with 1 short time series (that will error on wavelet) and 1 long
     df = frame_df.query("CellID == 30 | (CellID == 31 & FrameID <= 5)")
     output = time_series_features(df)
@@ -68,3 +69,4 @@ def test_time_series_features_1_short_1_long(frame_df):
     wavelet_cols = list(filter(wavelet_regex.search, output.columns.values))
     assert output.loc[output["CellID"] == 31, wavelet_cols].isna().all().all()
     assert ~output.loc[output["CellID"] == 30, wavelet_cols].isna().any().any()
+    assert output.shape[1] == ts_df.shape[1]  # Correct # cols
