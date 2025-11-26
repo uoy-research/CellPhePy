1d4e61b797203ed4694967aa1021260afeaeecc7
Stuart Lacy <stuart.lacy@york.ac.uk>
2025-11-26 12:02:46 +0000

    Hacky way to import optional modules at import time
    Create a dummy function that wraps the importing logic, so calling code
    can still access the functions with the top-level import, they just
    aren't imported at module first import.

--- src/cellphe/__init__.py
+++ src/cellphe/__init__.py
@@ -12,6 +12,16 @@ from cellphe.clustering import identify_clusters as identify_clusters
 from cellphe.features import cell_features as cell_features
 from cellphe.features import time_series_features as time_series_features
 from cellphe.input import import_data as import_data
-from cellphe.input import segment_images as segment_images
-from cellphe.input import track_images as track_images
 from cellphe.separation import calculate_separation_scores as calculate_separation_scores
+
+
+def segment_images():
+    from cellphe.segmentation import segment_images as dummy
+
+    return dummy
+
+
+def track_images():
+    from cellphe.tracking import track_images as dummy
+
+    return dummy
