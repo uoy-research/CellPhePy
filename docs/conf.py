e40573d635f2f314c401013a78354dc361b9dd5a
Stuart Lacy <stuart.lacy@york.ac.uk>
2026-04-30 14:09:47 +0100

    Applied Shibuya theme

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "shibuya"
+html_title = "CellPhe Project"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -67,6 +68,8 @@ html_static_path = ["_static"]
 html_css_files = [
     "custom.css",
 ]
+
+html_theme_options = {"accent_color": "violet", "color_mode": "light"}
 
 # Napoleon settings
 napoleon_include_init_with_doc = True
